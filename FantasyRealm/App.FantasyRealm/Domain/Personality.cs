3568abdffb5eda8fefebcaba4c807ec1106181bc
Boran Deniz <153175295+BrnDnzDzgn@users.noreply.github.com>
2025-02-18 08:43:08 -0800

    name field added, nullable disabled

--- FantasyRealm/App.FantasyRealm/Domain/Personality.cs
+++ FantasyRealm/App.FantasyRealm/Domain/Personality.cs
@@ -1,10 +1,12 @@
 ﻿
 using Core.App.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace App.FantasyRealm
 {
     public class Personality: Entity 
     {
-
+        [Required, StringLength(125)]
+        public string Name { get; set; }
     }
 }
